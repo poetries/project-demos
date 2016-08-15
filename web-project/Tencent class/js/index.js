71ebd804fbda92f56134dd14fb0e5e243e601379
poetries <jingguanliuye@gmail.com>
2016-08-15 23:08:25 +0800

    update docs

--- web-project/Tencent class/js/index.js
+++ web-project/Tencent class/js/index.js
@@ -1,9 +1,16 @@
 $(function(){
+	//weixinICON
 	var $button = $("#banner div.content div.nav-right-board div.button");
 	$button.mouseover(function(){
 		$(this).find("img",$(this)).show();
 	});
 	$button.mouseout(function(){
 		$(this).find("img",$(this)).hide();
+	});
+
+	//hot-course-tab
+	$("div.content div.course-nav ul.course-type li").mouseover(function(){
+		$(this).addClass("curr").siblings().removeClass("curr");
+		$($("a",$(this)).attr("for")).addClass("on").siblings().removeClass("on");
 	});
 });
