33835f49b0c0c4bea5922f479dd12f6a4479c863
poetries <jingguanliuye@gmail.com>
2016-09-01 01:24:33 +0800

    update docs

--- web-project/Mi-website/js/index.js
+++ web-project/Mi-website/js/index.js
@@ -11,19 +11,24 @@ $("#mi-topbar .topbar-cart").hover(function(){
 $("#header .header-search form").find(".txt").focus(function(event) {
 	$(this).parents("form").find(".show-list").fadeIn(400);
 	$(this).parents("form").find(".border").removeClass("border").addClass('on');
+	$(this).parents("form").find("p").hide();
 	
 }).blur(function(event) {
 	/* Act on the event */
 	$(this).parents("form").find(".show-list").fadeOut(400);
 	$(this).parents("form").find(".on").removeClass("on").addClass("border");
-	
+	$(this).parents("form").find("p").show();
 });
 
 //隐藏的菜单
 
-// $("#header").find("ul li").hover(function() {
-// 	$(this).parents("#header").find(".header-details ol li dl").show();
+$("#header").find("ul li").hover(function() {
+	$(this).parents("#header").find(".header-details ol li").eq($(this).index()).fadeIn(200).siblings().hide();
+	$(this).parents("#header").addClass('on');
+	$(this).parents("#header").find(".detail-bg").show();
 
-// },function(){
-// 	$(this).parents("#header").find(".header-details ol li dl").hide();
-// });
+},function(){
+	$(this).parents("#header").find(".header-details ol li").fadeOut(200);
+	$(this).parents("#header").removeClass('on');
+	$(this).parents("#header").find(".detail-bg").hide();
+});
