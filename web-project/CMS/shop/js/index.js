a10714b06eedc457cc39bd5b3ae61db1338fc975
poetries <jingguanliuye@gmail.com>
2016-11-02 21:42:03 +0800

    update docs

--- web-project/CMS/shop/js/index.js
+++ web-project/CMS/shop/js/index.js
@@ -132,3 +132,55 @@ $("#topEvent .topList .topList-r p.ti a ").click(function(){
 	$("#topEvent .topList .topList-r-recommend .con").eq(_index).fadeIn(300).siblings().fadeOut(300);
 });
 
+//个性定制
+/*var _index = 0;
+var oTimer = null;
+var oLi = $("#specific .parCon .parL .imgList ul li").size();
+
+function oBtnNext(){
+	_index++;
+
+	if (_index==oLi)
+	{
+		_index=0;
+	}
+	oBtnDo();
+}
+function oBtnDo(){
+	$("#specific .parCon .parL .imgList ul li").css("left","0px").eq(_index).fadeIn(200).siblings().fadeOut(200);
+}
+
+$("#specific .parCon .parL a.prev").click(function(){//prev
+	_index--;
+
+	if (_index<0)
+	{
+		_index=oLi-1;
+	}
+	oBtnDo();
+
+});
+
+$("#specific .parCon .parL a.next").click(function(){//next
+	oBtnNext();
+	oBtnDo();
+});
+
+
+$("#specific .parCon .parL").hover(function(){
+	clearInterval(oTimer);
+},function(){
+	oTimer = setInterval(oBtnNext,4000);
+});*/
+
+// 个性定制 右
+$("#specific .parCon .parM ul li ").not(".last").mouseover(function(){
+	var _index = $(this).index();
+	$(this).addClass("active").siblings().removeClass("active");
+	$("#specific .parCon .parR ul.con").eq(_index).fadeIn(300).siblings().fadeOut(300);
+});
+$("#gift .parCon .parM ul li ").not(".last").mouseover(function(){
+	var _index = $(this).index();
+	$(this).addClass("active").siblings().removeClass("active");
+	$("#gift .parCon .parR ul.con").eq(_index).fadeIn(300).siblings().fadeOut(300);
+});
