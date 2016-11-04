a60f2cb316ebe39145abe733bc48368bee1e9f9b
poetries <jingguanliuye@gmail.com>
2016-11-04 12:03:03 +0800

    update docs

--- web-project/CMS/shop/js/index.js
+++ web-project/CMS/shop/js/index.js
@@ -205,3 +205,10 @@ $("#gift .parCon .parM ul li ").not(".last").mouseover(function(){
 	$("#gift .parCon .parR ul.con").eq(_index).fadeIn(300).siblings().fadeOut(300);
 });
 
+//登录注册
+
+$("#login .content p.ti span").mouseover(function(){
+	var _index = $(this).index();
+	$(this).addClass("active").siblings().removeClass("active");
+	$("#login .content .LgRegCon .con").eq(_index).show().siblings().hide();
+});
