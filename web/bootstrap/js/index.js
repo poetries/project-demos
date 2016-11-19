7c3ed6e810e9eab90ede7f4019fd49cde1ff651c
poetries <jingguanliuye@gmail.com>
2016-11-20 00:23:52 +0800

    update

--- web/bootstrap/js/index.js
+++ web/bootstrap/js/index.js
@@ -9,14 +9,16 @@ $(window).bind("scroll",function () {
 
 });
 
-$("#g-topbar .navbar-nav li").click(function(){
-});
+
 $(".team-member").owlCarousel({
     slideSpeed:200,
     paginationSpeed:400,
     items : 4,
     lazyLoad : true
 });
+
+
+
 var $container = $("#lightBox");
 $container.isotope({
     filter:"*",
@@ -24,10 +26,29 @@ $container.isotope({
         duration:750,
         easing:"linear"
     }
+
 });
-$("#cat a").click(function(){
-    $("#cat a").removeClass("active");
-    $(this).addClass("active");
+$('body').scrollspy({
+    target: '.navbar-default',
+    offset: 80
+});
+$("a.page-scroll").click(function () {
+    $(this).parents().addClass("active").siblings().removeClass("active");
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+        }
+    }
+});
+
+$(".cate-title .type a").mouseover(function () {
+
+    $(this).addClass("active").parent().siblings().find("a").removeClass("active");
     var selector = $(this).attr("data-filter");
     $container.isotope({
         filter:selector,
